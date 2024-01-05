38246a7a16b4bc77f50050b5e68be0d9acca43d2
hemapot <155512544+hemapot@users.noreply.github.com>
2024-01-05 15:07:40 +1100

    Update Streamlit_app.py
    fix filter

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
+import pandas as pd
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -10,13 +10,18 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Set the index of the DataFrame to the 'Fruit' column
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Display the original DataFrame
+st.dataframe(my_fruit_list)
+
+# Create a multiselect widget to pick some fruits
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+# Filter the original DataFrame to only include the selected fruits
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(fruits_to_show)
