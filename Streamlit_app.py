80e9526f3b3af5d9b673a6249f3163df3228b717
hemapot <155512544+hemapot@users.noreply.github.com>
2024-01-05 17:22:34 +1100

    Update Streamlit_app.py
    Multiselect fix

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -13,17 +13,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-# Set the index of the DataFrame to the 'Fruit' column
 my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
+streamlit.dataframe(my_fruit_list)
 
-# Display the original DataFrame
-st.dataframe(my_fruit_list)
-
-# Create a multiselect widget to pick some fruits
-fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-
-# Filter the original DataFrame to only include the selected fruits
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), key = 'Lime')
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the DataFrame containing only the selected fruits
-st.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)
+
