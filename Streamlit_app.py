4792b7b301387048f1ee09903dc3714ef66acb6b
hemapot <155512544+hemapot@users.noreply.github.com>
2024-01-09 17:42:52 +1100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -585,25 +585,9 @@ def icc_readiness_page():
             'ICC Level': ['Nil', 'B (I)', 'C (60)', 'Nil', 'B (I)', 'C (60)', 'Nil', 'B (I)', 'C (60)', 'Nil', 'B (I)', 'C (60)', 'Nil', 'B (I)', 'C (60)', 'Nil', 'B (I)', 'C (60)']
             }
         
-    #df3 = pd.dataframe(dummy_data_3)
+    df3 = pd.dataframe(dummy_data_3)
 
-    def highlight_Quality(row):
-        styles = []
-        for value in row:
-            if value == 'Low Moderate':
-                styles.append(f'background-color: Green;') # color: white
-            elif value == 'Moderate':
-                styles.append(f'background-color: yellow;') # color: black
-            elif value == 'High':
-                styles.append(f'background-color: orange; ') #color: white
-            elif value == 'Very High':
-                styles.append(f'background-color: Red; ') #color: white
-            elif value == 'Extreme':
-                styles.append(f'background-color: DarkRed;') # color: white
-            else:
-                styles.append('')
-        return styles    
-    st.dataframe(dummy_data_3.to_pandas().style.apply(highlight_Quality, axis = 1))
+ 
 
     
       
